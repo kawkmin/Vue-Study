bac7c85adf7f4fc2bb7433aaec9395daa9d28a85
kawksung <alstjd5479@naver.com>
2024-02-09 14:42:43 +0900

    compostion: compostion 기본 사용법

--- inflearn/vue3_compostion/vue-composition/src/App.vue
+++ inflearn/vue3_compostion/vue-composition/src/App.vue
@@ -1,47 +1,36 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <p>{{ message }}</p>
+    <button @click="changeMessage">change</button>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import { ref } from 'vue'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+export default {
+  // data() {
+  //   return {
+  //     message: 'hello'
+  //   }
+  // },
+  setup() {
+    //data
+    const message = ref('hello')
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    //methods
+    function changeMessage() {
+      message.value = 'hi'
+    }
+
+    return { message, changeMessage }
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+  // methods: {
+  //   changeMessage() {
+  //     this.message = 'hi'
+  //   }
+  // }
 }
-</style>
+</script>
+
+<style lang="scss" scoped></style>
