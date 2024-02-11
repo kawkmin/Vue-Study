397e54637a74e7e0fe394476a242a158ce34fdb2
kawksung <alstjd5479@naver.com>
2024-02-11 15:34:27 +0900

    compositon: 헤드 컴포넌트 구현 및 적용

--- inflearn/vue3_compostion/vue-intermediate/src/App.vue
+++ inflearn/vue3_compostion/vue-intermediate/src/App.vue
@@ -1,7 +1,15 @@
 <template>
-  <div></div>
+  <TodoHeader></TodoHeader>
 </template>
 
-<script setup></script>
+<script>
+import TodoHeader from '@/components/TodoHeader.vue'
+export default {
+  components: {
+    // 컴포넌트 이름 : 컴포넌트 내용
+    TodoHeader
+  }
+}
+</script>
 
 <style lang="scss" scoped></style>
---
+++ inflearn/vue3_compostion/vue-intermediate/src/components/TodoHeader.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>TODO 앱</h1>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
