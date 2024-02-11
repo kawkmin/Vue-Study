67e91fc4b662f8769c2248991090a2ce3499fe20
kawksung <alstjd5479@naver.com>
2024-02-11 16:39:02 +0900

    compositon: 할일 관리 리스트 컴포넌트 현

--- inflearn/vue3_compostion/vue-intermediate/src/App.vue
+++ inflearn/vue3_compostion/vue-intermediate/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <TodoHeader></TodoHeader>
   <TodoInput></TodoInput>
+  <TodoList></TodoList>
 </template>
 
 <script>
 import TodoHeader from '@/components/TodoHeader.vue'
 import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
 
 export default {
   components: {
     // 컴포넌트 이름 : 컴포넌트 내용
     TodoHeader,
-    TodoInput
+    TodoInput,
+    TodoList
   }
 }
 </script>
---
+++ inflearn/vue3_compostion/vue-intermediate/src/components/TodoList.vue
@@ -0,0 +1,14 @@
+<template>
+  <ul>
+    <li v-for="(item, index) in items" :key="index">{{ item }}</li>
+  </ul>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+//data
+const items = ref(['애플', '삼성', '인프런'])
+</script>
+
+<style lang="scss" scoped></style>
