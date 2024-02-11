8a5ce894258a6e78f340e17dd13f577fa8192ea6
kawksung <alstjd5479@naver.com>
2024-02-11 15:51:09 +0900

    compositon: 할일 인풋 컴포넌트 구현

--- inflearn/vue3_compostion/vue-intermediate/src/App.vue
+++ inflearn/vue3_compostion/vue-intermediate/src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <TodoHeader></TodoHeader>
+  <TodoInput></TodoInput>
 </template>
 
 <script>
 import TodoHeader from '@/components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+
 export default {
   components: {
     // 컴포넌트 이름 : 컴포넌트 내용
-    TodoHeader
+    TodoHeader,
+    TodoInput
   }
 }
 </script>
---
+++ inflearn/vue3_compostion/vue-intermediate/src/components/TodoInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input type="text" v-model="todoInput" />
+    <button @click="addTodo">추가</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+//data
+const todoInput = ref('')
+
+//methods
+const addTodo = () => {
+  const todo = todoInput.value
+  localStorage.setItem(todo, todo)
+}
+</script>
+
+<style lang="scss" scoped></style>
