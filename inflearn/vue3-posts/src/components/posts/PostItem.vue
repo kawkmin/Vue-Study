a7e818f5faa030f68bcf99dc615394f97f7824d7
kawksung <alstjd5479@naver.com>
2024-02-12 22:09:10 +0900

    post: router을 이용한 게시판 CRUD 연결 및 기본 구현

---
+++ inflearn/vue3-posts/src/components/posts/PostItem.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="card" style="width: 18rem">
+		<div class="card-body">
+			<h5 class="card-title">{{ title }}</h5>
+			<p class="card-text">
+				{{ content }}
+			</p>
+			<p class="text-muted">{{ createdAt }}</p>
+		</div>
+	</div>
+</template>
+
+<script setup>
+defineProps({
+	title: {
+		type: String,
+		required: true,
+	},
+	content: {
+		type: String,
+	},
+	createdAt: {
+		type: [String, Date, Number], // 셋 중 아무거나
+	},
+});
+</script>
+
+<style lang="scss" scoped></style>
